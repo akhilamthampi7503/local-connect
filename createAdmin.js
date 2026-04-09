3b4ee9f2541f060418d87acb5af900ee7af49570
Akhila <akhilamthampi7503>
2026-04-09 11:19:14 +0530

    toursit modified

--- createAdmin.js
+++ createAdmin.js
@@ -3,6 +3,6 @@ const bcrypt = require("bcryptjs");
 bcrypt.hash("admin123", 10).then(console.log);
 
 
-bcrypt.hash("test123", 10).then(console.log);
+bcrypt.hash("guide123", 10).then(console.log);
 
 bcrypt.hash("1234", 10).then(console.log);
